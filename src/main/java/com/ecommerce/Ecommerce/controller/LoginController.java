de4e80a0283494e1cc9e327f5e78924f393368ca
Neerajrathore017 <rathoreneeraj017@gmail.com>
2025-09-20 21:33:43 +0530

    Second push

--- src/main/java/com/ecommerce/Ecommerce/controller/LoginController.java
+++ src/main/java/com/ecommerce/Ecommerce/controller/LoginController.java
@@ -1,16 +1,29 @@
 package com.ecommerce.Ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.Ecommerce.modal.Users;
+import com.ecommerce.Ecommerce.service.UserService;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class LoginController {
 	
+	@Autowired
+	private UserService userservice;
+	
 	@GetMapping("/")
 	public String greet(HttpServletRequest req) {
 		return "Hello from Server "+ req.getSession().getId();
+	}
+	@PostMapping("/register")
+	public Users register(@RequestBody Users user) {
+		return userservice.register(user);
 	}
 	
 	
---
+++ src/main/java/com/ecommerce/Ecommerce/service/UserService.java
@@ -0,0 +1,22 @@
+package com.ecommerce.Ecommerce.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.Ecommerce.modal.Users;
+import com.ecommerce.Ecommerce.repo.UserRepo;
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepo repo;
+	private  BCryptPasswordEncoder encoder=new BCryptPasswordEncoder(12);
+	
+	public Users register(Users user) {
+		user.setPassword(encoder.encode(user.getPassword()));
+		return repo.save(user);
+		
+	};
+
+}
